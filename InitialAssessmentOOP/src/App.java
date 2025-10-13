783c0d363f993476ec200c8969a63b797420e362
1MariaAngeles <mhe181374@iesvirreymorcillo.es>
2025-10-13 14:16:08 +0200

    Comienzo

---
+++ InitialAssessmentOOP/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Products producto1 = new Products("pc", 299.9, 10);
+        Products producto2 = new Products("mouse", 19.99, 11);
+        Products producto3 = new Products("keyboard", 89.99, 9);
+
+    }
+}
